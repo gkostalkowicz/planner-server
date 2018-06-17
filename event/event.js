2d9ef5fe2838c3768116953339fa6d5916837e89
Grzegorz Kostalkowicz <g.kostalkowicz@gmail.com>
2018-06-17 21:02:55 +0200

    Server: Added date validation

--- event/event.js
+++ event/event.js
@@ -1,13 +1,24 @@
 var mongoose = require('mongoose');
+var js_joda = require('js-joda');
 
 const Schema = mongoose.Schema;
+const LocalDate = js_joda.LocalDate;
 
 const eventSchema = new Schema({
     _id: Number,
     name: {type: String, required: true},
-    start: {type: String, required: true}, // TODO add validation
-    end: {type: String, required: true}, // TODO add validation
+    start: {type: String, required: true, validate: {validator: isDateValid, message: '{VALUE} is not a valid date'}},
+    end: {type: String, required: true, validate: {validator: isDateValid, message: '{VALUE} is not a valid date'}},
     description: String
 });
+
+function isDateValid(dateString) {
+    try {
+        LocalDate.parse(dateString);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
 
 module.exports = mongoose.model('Event', eventSchema);
